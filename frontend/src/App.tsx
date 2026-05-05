73e032704e5d1bbf851c94786e995ade36d0975b
ced <cedrickced820@gmail.com>
2026-05-05 10:47:49 +0800

    Add react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import ListingPage from "./pages/ListingPage"
+import { Route, Routes } from 'react-router-dom';
+import ListingPage from "./pages/ListingPage"; 
+import ProductPage from './pages/ProductPage';
+
 
 function App() {
   return (
-    <>
-     <ListingPage />
-    </>
+    <Routes>
+     <Route path="/" element={ <ProductPage /> } />
+     <Route path="/listing" element={ <ListingPage /> } /> 
+    </Routes>
   )
 }
 
