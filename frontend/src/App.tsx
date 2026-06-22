6e02e8af46db69d87c32c59f5812b0447ce1bccf
ced <cedrickced820@gmail.com>
2026-06-22 17:59:25 +0800

    fix: path router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={ <RequestPage/> } />
-      <Route path="/TripPage" element={ <TripPage /> } /> 
+      <Route path="/trips" element={ <TripPage /> } /> 
     </Routes>
   )
 }
