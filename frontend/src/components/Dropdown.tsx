0986502d4bdca983c0cdad897f6e68770427317c
ced <cedrickced820@gmail.com>
2026-06-24 20:12:42 +0800

    refactor: dropdown link tag for each options

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -11,39 +11,13 @@ export default function Dropdown({mode,onClose}: { mode: string; onClose: () => 
   mode === 'create' || 'avatar' ? translate = '-translate-x-1/2': translate
   return (
     <>
-    <div className={`absolute left-1 right-1 mt-2 ${w} ${translate} w-fit  bg-white border border-gray-200 rounded-lg shadow-lg z-50 py-2 px-4`}>
+    <div className={`absolute left-1 right-1 mt-2 ${w} ${translate} w-40 bg-white border border-gray-200 rounded-lg shadow-lg z-50 py-2 px-4 flex flex-col`}>
+      
       {mode === "avatar" && (
         <>
-        <div className="flex items-center justify-center gap-0">
-
-          <div className="py-2">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="h-6 w-6"
-            >
-              <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-              <circle cx="12" cy="7" r="4" />
-            </svg>
-          </div>
-
-          <div className="px-1">
-            <Link to="/profile" onClick={onClose} className="block font-bold text-sm text-gray-700 hover:bg-gray-100">
-              Profile
-            </Link>
-          </div>
-
-        </div>
-
-          <div className="flex items-center justify-center gap-0">
-          
-          <div className="py-2">
-            <svg
+          <Link to="/profile" onClick={onClose} className="hover:bg-gray-100 py-1">
+            <div className="flex items-center justify-center gap-1 ">
+              <svg  
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
@@ -52,61 +26,75 @@ export default function Dropdown({mode,onClose}: { mode: string; onClose: () => 
               strokeLinecap="round"
               strokeLinejoin="round"
               className="h-5 w-5"
-            >
-              <circle cx="12" cy="12" r="3" />
-              <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
-            </svg>
-          </div>
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+            
+              <p className="block font-bold text-gray-700 text-sm ">Profile</p>
+            </div>
+          </Link>
 
-          <div className="px-1">
-            <Link to="/profile" onClick={onClose} className="block font-bold text-sm text-gray-700 hover:bg-gray-100">
-              Settings
-            </Link>
-          </div>
+          <Link to="/profile" onClick={onClose} className="hover:bg-gray-100 py-1">
+            <div className="flex items-center justify-center gap-1 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-5 w-5"
+              >
+                <circle cx="12" cy="12" r="3" />
+                <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
+              </svg>
+            
+              <p className="block font-bold text-gray-700 text-sm">Settings</p>
+            </div>
+          </Link>
 
-        </div>
-
-        <div className="flex items-center justify-center gap-0">
-
-          <div className="py-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-6 w-6"
-            >
-              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-              <polyline points="16 17 21 12 16 7" />
-              <line x1="21" y1="12" x2="9" y2="12" />
-            </svg>
-          </div>
-
-          <div className="px-1">
-            <Link to="/profile" onClick={onClose} className="block font-bold text-sm text-gray-700 hover:bg-gray-100">
-              Profile
-            </Link>
-          </div>
-
-        </div>
-          
+          <Link to="/profile" onClick={onClose} className="hover:bg-gray-100 py-1">
+            <div className="flex items-center justify-center gap-1 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="red"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="h-5 w-5"
+              >
+                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                <polyline points="16 17 21 12 16 7" />
+                <line x1="21" y1="12" x2="9" y2="12" />
+              </svg>
+            
+              <p className="block font-bold text-red-600 text-sm ">Sign Out</p>
+            </div>
+          </Link>
         </>
       )}
+
+
       {mode === 'notif' &&(
         <div className=" p-2 h- ">
           <div className="px-2 block"><h1>Notifications</h1></div>
-
-          <div className=" p-2">
+          
+          <Link to="/notification" onClick={onClose} className="hover:bg-gray-100 py-1">
+            <div className=" p-2">
              <div className="border py-4 w-full"><p className="text-[12px]">notif example</p></div>
-          </div>
+            </div>
+          </Link>
+          
         </div>
       )}
+
+
       {mode === 'create' &&(
         <>
-          
           <Link
             to="/request"
             onClick={onClose}
@@ -124,6 +112,7 @@ export default function Dropdown({mode,onClose}: { mode: string; onClose: () => 
           </Link>
         </>
       )}
+      
     </div>
     </>
   );
