9c4400c3b8d39d21a78a9af70e5169754700d227
ced <cedrickced820@gmail.com>
2026-06-29 21:55:26 +0800

    refactor: remove fisher-yates algorithm for UX purposes

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,40 +8,28 @@ import TripCard from "../components/TripCard";
 
 
 
-
-//fisher-yates algo for shuffling
-function shuffle(cards: (RequestResponse | Trip)[]){
-  for (let i = cards.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    
-    [cards[i], cards[j]] = [cards[j], cards[i]];
-  }
-  return cards;
-}
-
 export default function HomePage(){
     const reqData = usePull<RequestResponse>('http://localhost:5000/api/v1/request/getAll');  
     const tripData = usePull<Trip>('http://localhost:5000/api/v1/trips/getAll');
-
-    const cards = [ ...reqData, ...tripData]; 
-    const shuffledCards = shuffle(cards)
+    
   return(
      <>
-
     <Header username="current user"/>
 
     <section className="py-24">
+      {/* request */}
+      
+      <div className="columns-1 sm:columns-2 md:columns-3 xl:columns-4 2xl:columns-6 gap-4 px-16 ">
+        {reqData.map((card) =>(
+          <RequestCard key={card.request_id} { ...card } homeStyle="break-inside-avoid mb-4" /> 
+        ))}
+      </div>
       <div className="columns-1 sm:columns-2 md:columns-3 xl:columns-4 2xl:columns-6 gap-4 px-16">
-        {
-          shuffledCards.map((card) =>(
-            'request_id' in card ? 
-            <RequestCard key={card.request_id} { ...card } homeStyle="break-inside-avoid mb-4" /> 
-            : <TripCard key={card.trip_id} { ...card } homeStyle="break-inside-avoid mb-4"/>
-          ))
-        }
+        {tripData.map((card) =>(
+            <TripCard key={card.trip_id} { ...card } homeStyle="break-inside-avoid mb-4"/>
+        ))}
       </div>
     </section>
-
     </>
   )
 }
